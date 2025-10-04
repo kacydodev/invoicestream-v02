40df1af00f442d973606ae83099e523210c499b1
Chinh Do <chinh@doinby.co>
2025-10-04 13:11:35 +1000

    add react query provider and query functions

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,16 +1,20 @@
-import { useLocation } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import { getInvoices } from '../utils/queries/getInvoices';
+import ErrorPage from './ErrorPage';
 
 export default function DashboardPage() {
-	const params = useLocation();
-	return (
-		<main>
-			<pre>{JSON.stringify(params, null, 2)}</pre>
-		</main>
-	);
+	const { data, error } = useQuery({
+		queryKey: ['invoices'],
+		queryFn: getInvoices,
+	});
+
+	if (error instanceof Error) return <ErrorPage error={error} />;
+
 	return (
 		<>
 			<main>
 				Dashboard
+				<pre>{JSON.stringify(data, null, 2)}</pre>
 				{/* <Header count={count || 0} />
 				{error ? <Error error={error} /> : <Invoices data={data} />} */}
 			</main>

f1d7bab179838d242c2f50a8baa5c5c9a0059e1e
Kacy Do <contact@kacydo.dev>
2025-11-20 18:52:14 +1100

    add table styling

--- src/components/ui/DashboardTable.tsx
+++ src/components/ui/DashboardTable.tsx
@@ -5,19 +5,30 @@ import {
 	Column,
 	Row,
 	Cell,
+	Button,
 } from 'react-aria-components';
 import type { invoiceRowType } from '../../utils/types';
+import classNames from 'classnames';
+import { ChevronRight } from 'lucide-react';
 
 export function DashboardTable({ invoices }: { invoices: invoiceRowType[] }) {
 	const columnItems = Object.keys(invoices[0]).filter(
 		(item) => item !== 'description'
 	);
-	return (
-		<Table aria-label='Invoices Table' className='w-full'>
-			<TableHeader className=''>
-				{/* <Column>Header 1</Column>
-					<Column isRowHeader>Header 2</Column> */}
 
+	const tableClassName = classNames({
+		'w-full': false,
+		'border-separate border-spacing-y-3': true,
+		'[&_td,_&_th]:px-4 [&_td,_&_th]:py-3': true,
+	});
+
+	return (
+		<Table
+			aria-label='Invoices Table'
+			selectionMode='multiple'
+			className={tableClassName}
+		>
+			<TableHeader className='text-left'>
 				{columnItems.map((item, idx) => {
 					return (
 						<Column key={item} isRowHeader={idx === 0} className=''>
@@ -25,8 +36,9 @@ export function DashboardTable({ invoices }: { invoices: invoiceRowType[] }) {
 						</Column>
 					);
 				})}
+				<Column />
 			</TableHeader>
-			<TableBody>
+			<TableBody className='bg-primary-darker text-left'>
 				{invoices.map((invoice) => {
 					const {
 						secondaryId,
@@ -36,12 +48,17 @@ export function DashboardTable({ invoices }: { invoices: invoiceRowType[] }) {
 						client: { name },
 					} = invoice;
 					return (
-						<Row key={secondaryId}>
+						<Row key={secondaryId} className=''>
 							<Cell>{secondaryId}</Cell>
 							<Cell>{paymentDue}</Cell>
 							<Cell>{status}</Cell>
 							<Cell>{total}</Cell>
 							<Cell>{name}</Cell>
+							<Cell>
+								<Button>
+									<ChevronRight />
+								</Button>
+							</Cell>
 						</Row>
 					);
 				})}

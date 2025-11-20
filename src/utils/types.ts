169f27bd568cbdca47db2b255708818222ac076a
Kacy Do <contact@kacydo.dev>
2025-11-20 13:16:04 +1100

    add invoice and client zod types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,4 @@
-// export type ThemeType = 'light' | 'dark';
-
+import { z } from 'zod';
 import type { SetURLSearchParams } from 'react-router';
 
 export const themeType = ['light', 'dark'];
@@ -12,3 +11,49 @@ export interface DashboardPropsInterface {
 	params?: URLSearchParams;
 	setParams: SetURLSearchParams;
 }
+
+const statusZod = z.enum(['draft', 'pending', 'paid']);
+
+export const InvoiceFull = z.object({
+	id: z.uuid(),
+	secondaryId: z.string(),
+	createdAt: z.iso.datetime(),
+	updateAt: z.optional(z.iso.datetime()),
+	description: z.string(),
+	paymentTerm: z.optional(z.int()),
+	paymentDue: z.optional(z.iso.datetime()),
+	status: statusZod,
+	client: {
+		id: z.uuid(),
+		name: z.string(),
+		email: z.email(),
+		addres: z.string(),
+	},
+	items: {
+		id: z.uuid(),
+		title: z.string(),
+		quantity: z.int32(),
+		price: z.float32(),
+	},
+	total: z.optional(z.float64()),
+});
+export const Invoice = z.object({
+	secondaryId: z.string(),
+	description: z.optional(z.string()),
+	paymentDue: z.optional(z.iso.datetime()),
+	status: statusZod,
+	client: { name: z.string() },
+	total: z.optional(z.float64()),
+});
+export const Client = z.object({
+	id: z.uuid(),
+	name: z.string(),
+	email: z.email(),
+	addres: z.string(),
+});
+export const Item = z.object({
+	id: z.uuid(),
+	title: z.string(),
+	quantity: z.int32(),
+	price: z.float32(),
+});

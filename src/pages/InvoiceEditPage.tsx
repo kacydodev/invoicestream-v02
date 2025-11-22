993cb79be205145df19cb63cdd6f2be071dbd684
Kacy Do <contact@kacydo.dev>
2025-11-22 14:54:17 +1100

    add InvoiceEditForm with React Hook Form + Zod

--- src/pages/InvoiceEditPage.tsx
+++ src/pages/InvoiceEditPage.tsx
@@ -1,10 +1,41 @@
-import { useParams } from 'react-router';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { InvoiceForm, type InvoiceFormType } from '../utils/types';
+import { FormInput } from '../components/form/FormInput';
+import { MyButton } from '../components/ui/MyButton';
+
+// /invoice/edit/0e7cee1e-e78d-44d2-87da-24a45b985f81
 
 export default function InvoiceEditPage() {
-	const params = useParams();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<InvoiceFormType>({ resolver: zodResolver(InvoiceForm) });
+
+	const onSubmit: SubmitHandler<InvoiceFormType> = (data) => console.log(data);
 	return (
 		<main>
-			<pre>{JSON.stringify(params, null, 2)}</pre>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='p-8 space-y-6 rounded border border-ui-border'
+			>
+				<FormInput
+					label='Invoice ID'
+					id='secondaryId'
+					error={errors.secondaryId}
+					{...register('secondaryId')}
+				/>
+
+				<FormInput
+					id='description'
+					error={errors.description}
+					{...register('description')}
+				/>
+
+				<MyButton type='submit'>Submit</MyButton>
+			</form>
+			{/* {errors && <pre>{JSON.stringify(errors.email?.message, null, 2)}</pre>} */}
 		</main>
 	);
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,7 +6,7 @@ export type ThemeType = (typeof themeType)[number];
 // export const status = ['draft', 'pending', 'paid'];
 // export type StatusType = (typeof status)[number];
 
-const statusZod = z.enum(['draft', 'pending', 'paid']);
+export const Status = z.enum(['draft', 'pending', 'paid']);
 
 export const InvoiceFull = z.object({
 	id: z.uuid(),
@@ -16,7 +16,7 @@ export const InvoiceFull = z.object({
 	description: z.string(),
 	paymentTerm: z.optional(z.int()),
 	paymentDue: z.optional(z.iso.datetime()),
-	status: statusZod,
+	status: Status,
 	client: {
 		id: z.uuid(),
 		name: z.string(),
@@ -40,12 +40,10 @@ export const InvoiceRow = z.object({
 	secondaryId: z.string(),
 	description: z.optional(z.string()),
 	paymentDue: z.optional(z.iso.datetime()),
-	status: statusZod,
+	status: Status,
 	client: ClientRow,
 	total: z.optional(z.float64()),
 });
-
-export type InvoiceRowType = z.infer<typeof InvoiceRow>;
 
 export const Client = z.object({
 	id: z.uuid(),
@@ -60,3 +58,23 @@ export const Item = z.object({
 	quantity: z.int32(),
 	price: z.float32(),
 });
+
+export const InvoiceForm = z.object({
+	// id: z.uuid(),
+	secondaryId: z.optional(z.string().min(6)),
+	// createdAt: z.iso.datetime(),
+	// updateAt: z.optional(z.iso.datetime()),
+	description: z.optional(z.string().min(3)),
+	paymentTerm: z.optional(z.int()),
+	paymentDue: z.optional(z.iso.datetime()),
+	status: z.optional(Status),
+	// client: Client,
+	// items: Item,
+	// total: z.optional(z.float64()),
+});
+export const InvoiceFormId = InvoiceForm.keyof();
+
+export type StatusType = z.infer<typeof Status>;
+export type InvoiceRowType = z.infer<typeof InvoiceRow>;
+export type InvoiceFormType = z.infer<typeof InvoiceForm>;
+export type InvoiceFormIdType = z.infer<typeof InvoiceFormId>;
